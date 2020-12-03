ddc1cec029f977ad5835be10b1f085e694118525
Sean Fenelon <sean.fenelon93@gmail.com>
2020-12-03 13:39:47 +0000

    same page

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from environment.config import db_URI
-from flask_sqlalchemy import flask_sqlalchemy
-from flask_marshamllow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 bcrypt = Bcrypt(app)
