2a7c7e65d2bdc53d169e2caaf3b720cfb743101e
Sean Fenelon <sean.fenelon93@gmail.com>
2020-12-06 11:10:34 +0000

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import bulma from 'bulma'
 
 import NavBar from './components/Navbar'
 import Login from './components/Login'
+import Home from './components/Home'
 
 const App = () => {
   return <>
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return <h1>
+    Hello World
+  </h1>
+}
+
+export default Home
